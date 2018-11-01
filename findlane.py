460b7cb7fb8acc2f24554433a00a5c24695f5d94
Fei-Fei Zheng <feifeizheng@outlook.com>
2018-11-01 13:17:16 -0400

    extract lines with threshold

--- findlane.py
+++ findlane.py
@@ -1,3 +1,6 @@
+'''
+    project for finding lane on input image.
+'''
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
@@ -8,11 +11,22 @@ img = mpimg.imread('test_images/solidWhiteRight.jpg')
 
 # pirnt out info of input image and plot input image
 print('This image is: ', type(img), 'with dimensions: ', img.shape)
-plt.imshow(img)
-plt.show()
 
 # get x and y value of image 
-y = img.size[0]
-x = img.size[1]
+y = img.shape[0]
+x = img.shape[1]
 
 imCopy = np.copy(img)
+
+# set threshold to extract the lines
+th = [200, 200, 200]
+thresholds = (img[:, :, 0] < th[0]) | (img[:, :, 1] < th[1]) | (img[:, :, 2] < th[2])
+imCopy[thresholds] = [0, 0, 0]
+
+#plot both original image and line extracted image
+plt.figure(figsize=(6, 4))
+plt.subplot(211)
+plt.imshow(img)
+plt.subplot(212)
+plt.imshow(imCopy)
+plt.show()
