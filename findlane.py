8bd51977325295c20410ddfa03f8e85b6753e37b
Fei-Fei Zheng <feifeizheng@outlook.com>
2018-11-01 11:46:13 -0400

    get info of image

--- findlane.py
+++ findlane.py
@@ -3,10 +3,16 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2
 
-#read input image
+# read input image
 img = mpimg.imread('test_images/solidWhiteRight.jpg')
 
-#pirnt out info of input image and plot input image
+# pirnt out info of input image and plot input image
 print('This image is: ', type(img), 'with dimensions: ', img.shape)
 plt.imshow(img)
 plt.show()
+
+# get x and y value of image 
+y = img.size[0]
+x = img.size[1]
+
+imCopy = np.copy(img)
